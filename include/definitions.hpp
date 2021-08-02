f159512c4319c93fb4307725bb0297d3743a2eca
Douglas Friedel <friededn@yahoo.com>
2021-08-02 10:41:30 -0500

    added macro to create static accessors for a variable

--- include/definitions.hpp
+++ include/definitions.hpp
@@ -110,6 +110,12 @@ enum RequestedDebug : uint {None = 0,
 #define DEBUG_DELTA(x) do{}while(0);
 #define DEBUG_INTERNAL(x) do{}while(0);
 #endif
+
+#define SET_GET(NAME) public:\
+    static double get ## NAME() {return NAME;}\
+    static void set ## NAME(const double val) {NAME = val;}\
+    private:\
+        static double NAME;
 // rename a common data type to make it easier
 typedef std::vector<double> arr;
 
