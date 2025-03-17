e51bcfc42917f9cda0af1a8753e8b957281ffaca
Meagan Lang <langmm.astro@gmail.com>
2025-03-17 17:45:19 -0400

    Fix default shell for docs job Revert to using Ninja for the windows conda recipe build Add yggdrasil to the build requirements in the conda recipe

--- recipe/bld_cpp.bat
+++ recipe/bld_cpp.bat
@@ -4,23 +4,25 @@ set CONFIGURATION=Release
 mkdir conda_build
 cd conda_build
 
-:: Call cmake
+rem Call cmake
 rem -GNinja ^
 rem -D CMAKE_BUILD_TYPE=%CONFIGURATION% ^
 rem -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-cmake -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
-      -D WITH_YGGDRASIL:BOOL=ON ..
+cmake -G "Ninja" ^
+      -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+      -D WITH_YGGDRASIL:BOOL=ON ^
+      -D "Python_EXECUTABLE:FILEPATH=%PYTHON%" ..
 if errorlevel 1 exit 1
 
-# Build all, run tests, then install
+rem Build all, run tests, then install
 cmake --build . --config %CONFIGURATION%
 cmake --install .
 
-rem :: Using ninja
+rem Using ninja
 rem ninja all
 rem ninja install
 
-:: Run tests
+rem Run tests
 if errorlevel 1 exit 1
 ctest -C "%Configuration%" -V
 if errorlevel 1 exit 1
