4197de84c5bdf1df7341e5620297351932e74e7b
Meagan Lang <langmm.astro@gmail.com>
2025-11-03 18:49:58 -0600

    Added stdlib to conda recipe & varients file Set build to ninja on all platforms Better flag parsing Fix import name for python test

--- recipe/bld_cpp.bat
+++ recipe/bld_cpp.bat
@@ -4,23 +4,16 @@ set CONFIGURATION=Release
 mkdir conda_build
 cd conda_build
 
-rem Call cmake
-rem -GNinja ^
-rem -D CMAKE_BUILD_TYPE=%CONFIGURATION% ^
-rem -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
 cmake -G "Ninja" ^
       -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
       -D WITH_YGGDRASIL:BOOL=ON ^
-      -D "Python_EXECUTABLE:FILEPATH=%PYTHON%" ..
+      -D "Python_EXECUTABLE:FILEPATH=%PYTHON%" ^
+      ..
 if errorlevel 1 exit 1
 
 rem Build all, run tests, then install
 cmake --build . --config %CONFIGURATION%
 cmake --install .
-
-rem Using ninja
-rem ninja all
-rem ninja install
 
 rem Run tests
 if errorlevel 1 exit 1
--- recipe/build_cpp.sh
+++ recipe/build_cpp.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 set -euo pipefail
 
-# Using cmake
 if [ ! -d conda_build ]; then
     mkdir conda_build
 fi
 cd conda_build
-# -D CMAKE_BUILD_TYPE=Release \
-# -D CMAKE_INSTALL_PREFIX=$PREFIX \
 cmake ${CMAKE_ARGS} \
-    -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-    -D WITH_YGGDRASIL:BOOL=ON ..
+      -G "Ninja" \
+      -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+      -D WITH_YGGDRASIL:BOOL=ON \
+      ..
 
 # Build all, run tests, then install
 cmake --build . --config Release
 ctest -V
-cmake --install .
 
-# Make all, run tests, then install
-# make all VERBOSE=1
-# ctest -V
-# make install
+cmake --install .
 
 cd ..
